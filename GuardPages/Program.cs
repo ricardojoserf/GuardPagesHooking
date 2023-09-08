b50b352ad4786a722fdbed821702f6026f5f2ae0
Ricardo Ruiz <ricardojruiz93@gmail.com>
2023-09-08 08:31:57 +0200

    Add files via upload

--- GuardPages/Program.cs
+++ GuardPages/Program.cs
@@ -11,7 +11,6 @@ namespace GuardPages
         [DllImport("kernel32", CharSet = CharSet.Ansi, ExactSpelling = true, SetLastError = true)] static extern IntPtr GetProcAddress(IntPtr hModule, string procName);
         [DllImport("kernel32.dll")] static extern bool VirtualProtectEx(IntPtr hProcess, IntPtr lpAddress, UIntPtr dwSize, uint flNewProtect, out uint lpflOldProtect);
         [DllImport("kernel32.dll", SetLastError = true)] public static extern IntPtr GetCurrentProcess();
-        // PVOID WINAPI AddVectoredExceptionHandler(_In_ ULONG FirstHandler, _In_ PVECTORED_EXCEPTION_HANDLER VectoredHandler);
         [DllImport("kernel32.dll")] public static extern IntPtr AddVectoredExceptionHandler(uint FirstHandler, IntPtr VectoredHandler_POINTER);
         [DllImport("kernel32.dll", SetLastError = true)] static extern bool ReadProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, [Out] byte[] lpBuffer, int dwSize, out IntPtr lpNumberOfBytesRead);
         [DllImport("User32.dll")] public static extern int MessageBox(int h, string m, string c, int type);
@@ -26,7 +25,7 @@ namespace GuardPages
         public delegate long hookDel(IntPtr ExceptionInfo_Pointer);
         public delegate void testDel();
 
-
+        // Source: https://learn.microsoft.com/en-us/windows/win32/api/winnt/ns-winnt-exception_pointers
         public struct EXCEPTION_POINTERS
         {
             public EXCEPTION_RECORD exceptionRecord;
@@ -34,18 +33,7 @@ namespace GuardPages
 
         }
 
-        // https://learn.microsoft.com/en-us/windows/win32/api/winnt/ns-winnt-exception_record
-        /*
-        typedef struct _EXCEPTION_RECORD64 {
-            DWORD    ExceptionCode;
-            DWORD ExceptionFlags;
-            DWORD64 ExceptionRecord;
-            DWORD64 ExceptionAddress;
-            DWORD NumberParameters;
-            DWORD __unusedAlignment;
-            DWORD64 ExceptionInformation[EXCEPTION_MAXIMUM_PARAMETERS];
-        } EXCEPTION_RECORD64, *PEXCEPTION_RECORD64;
-        */
+        // Source: https://learn.microsoft.com/en-us/windows/win32/api/winnt/ns-winnt-exception_record
         public struct EXCEPTION_RECORD
         {
             public uint ExceptionCode;
@@ -53,11 +41,10 @@ namespace GuardPages
             public IntPtr pExceptionRecord;
             public IntPtr ExceptionAddress;
             public uint NumberParameters;
-            // public uint unusedAlignment;
             public IntPtr ExceptionInformation;
         }
 
-        // https://learn.microsoft.com/en-us/windows/win32/api/winnt/ns-winnt-context       
+        // Source: https://learn.microsoft.com/en-us/windows/win32/api/winnt/ns-winnt-context       
         public struct CONTEXT
         {
             public UInt64 P1Home;
@@ -101,93 +88,9 @@ namespace GuardPages
             public UInt64 R13;
             public UInt64 R14;
             public UInt64 R15;
-            public IntPtr Rip;
-            //    public anon0[512] byte,
-            /*
-            union {
-            XMM_SAVE_AREA32 FltSave,
-            NEON128 Q[16],
-            ULONGLONG D[32],
-            struct {
-            M128A Header[2],
-            M128A Legacy[8],
-            M128A Xmm0,
-            M128A Xmm1,
-            M128A Xmm2,
-            M128A Xmm3,
-            M128A Xmm4,
-            M128A Xmm5,
-            M128A Xmm6,
-            M128A Xmm7,
-            M128A Xmm8,
-            M128A Xmm9,
-            M128A Xmm10,
-            M128A Xmm11,
-            M128A Xmm12,
-            M128A Xmm13,
-            M128A Xmm14,
-            M128A Xmm15,
-            } DUMMYSTRUCTNAME,
-            UInt32 S[32],
-            } DUMMYUNIONNAME,
-            public M128A VectorRegister[26],
-            public UInt64 VectorControl,
-            public UInt64 DebugControl,
-            public UInt64 LastBranchToRip,
-            public UInt64 LastBranchFromRip,
-            public UInt64 LastExceptionToRip,
-            public UInt64 LastExceptionFromRip
-            */
+            public UInt64 Rip;
         };
-    
 
-        /*
-        // https://learn.microsoft.com/en-us/windows/win32/api/winnt/ns-winnt-context   
-        public struct CONTEXT
-        {
-            public IntPtr P1Home;
-            public IntPtr P2Home;
-            public IntPtr P3Home;
-            public IntPtr P4Home;
-            public IntPtr P5Home;
-            public IntPtr P6Home;
-            // Control Flags
-            public uint ContextFlags;
-            public uint MxCsr;
-            // Segment Register and Processor Flags
-            public ushort SegCs;
-            public ushort SegDs;
-            public ushort SegEs;
-            public ushort SegFs;
-            public ushort SegGs;
-            public ushort SegSs;
-            public uint EFlags;
-            // Debug Registers
-            public IntPtr Dr0;
-            public IntPtr Dr1;
-            public IntPtr Dr2;
-            public IntPtr Dr3;
-            public IntPtr Dr6;
-            public IntPtr Dr7;
-            // Registers
-            public IntPtr Rax;
-            public IntPtr Rcx;
-            public IntPtr Rdx;
-            public IntPtr Rbx;
-            public IntPtr Rsp;
-            public IntPtr Rbp;
-            public IntPtr Rsi;
-            public IntPtr Rdi;
-            public IntPtr R8;
-            public IntPtr R9;
-            public IntPtr R10;
-            public IntPtr R11;
-            public IntPtr R12;
-            public IntPtr R13;
-            public IntPtr R14;
-            public IntPtr R15;
-            public IntPtr Rip;
-        };*/
 
         private static T MarshalBytesTo<T>(byte[] bytes)
         {
@@ -197,24 +100,17 @@ namespace GuardPages
             return theStructure;
         }
 
-        public static void test() {
-            Console.WriteLine("[+] Message after hooking");
-            Debug.WriteLine("[+] Debug 2");
-            return;
-        }
 
-        static void printDebugInfo(EXCEPTION_POINTERS ExceptionInfo) {
+        static void printDebugInfo(EXCEPTION_POINTERS ExceptionInfo)
+        {
             Console.WriteLine("\n[+] Debug Information...");
             Console.WriteLine("[+] ExceptionInfo.exceptionRecord.ExceptionCode:        0x{0}", ExceptionInfo.exceptionRecord.ExceptionCode.ToString("X"));
             Console.WriteLine("[+] ExceptionInfo.exceptionRecord.ExceptionFlags:       0x{0}", ExceptionInfo.exceptionRecord.ExceptionFlags.ToString("X"));
             Console.WriteLine("[+] ExceptionInfo.exceptionRecord.pExceptionRecord:     0x{0}", ExceptionInfo.exceptionRecord.pExceptionRecord.ToString("X"));
             Console.WriteLine("[+] ExceptionInfo.exceptionRecord.ExceptionAddress:     0x{0}", ExceptionInfo.exceptionRecord.ExceptionAddress.ToString("X"));
             Console.WriteLine("[+] ExceptionInfo.exceptionRecord.NumberParameters:     0x{0}", ExceptionInfo.exceptionRecord.NumberParameters.ToString("X"));
-            // Console.WriteLine("[+] ExceptionInfo.exceptionRecord.unusedAlignment:      0x{0}", ExceptionInfo.exceptionRecord.unusedAlignment.ToString("X"));
-            Console.WriteLine("[+] ExceptionInfo.exceptionRecord.ExceptionInformation: 0x{0}",   ExceptionInfo.exceptionRecord.ExceptionInformation.ToString("X"));
-            
+            Console.WriteLine("[+] ExceptionInfo.exceptionRecord.ExceptionInformation: 0x{0}", ExceptionInfo.exceptionRecord.ExceptionInformation.ToString("X"));
             Console.WriteLine("");
-
             Console.WriteLine("[+] ExceptionInfo.contextRecord.P1Home:                 0x{0}", ExceptionInfo.contextRecord.P1Home.ToString("X"));
             Console.WriteLine("[+] ExceptionInfo.contextRecord.P2Home:                 0x{0}", ExceptionInfo.contextRecord.P2Home.ToString("X"));
             Console.WriteLine("[+] ExceptionInfo.contextRecord.P3Home:                 0x{0}", ExceptionInfo.contextRecord.P3Home.ToString("X"));
@@ -253,12 +149,12 @@ namespace GuardPages
             Console.WriteLine("[+] ExceptionInfo.contextRecord.R14:                    0x{0}", ExceptionInfo.contextRecord.R14.ToString("X"));
             Console.WriteLine("[+] ExceptionInfo.contextRecord.R15:                    0x{0}", ExceptionInfo.contextRecord.R15.ToString("X"));
             Console.WriteLine("[+] ExceptionInfo.contextRecord.Rip:                    0x{0}", ExceptionInfo.contextRecord.Rip.ToString("X"));
-
             Console.WriteLine("");
         }
 
 
-        static long hhandler(IntPtr ExceptionInfo_Pointer) {
+        static long handler_function(IntPtr ExceptionInfo_Pointer)
+        {
             IntPtr hProcess = Process.GetCurrentProcess().Handle;
             byte[] data1 = new byte[Marshal.SizeOf(typeof(IntPtr))];
             ReadProcessMemory(hProcess, ExceptionInfo_Pointer, data1, data1.Length, out _);
@@ -270,26 +166,13 @@ namespace GuardPages
 
             if (ExceptionInfo.exceptionRecord.ExceptionCode == STATUS_GUARD_PAGE_VIOLATION)
             {
-                printDebugInfo(ExceptionInfo);
+                //printDebugInfo(ExceptionInfo);
                 Console.WriteLine("[+] Captured exception with exception code:\t0x{0}", ExceptionInfo.exceptionRecord.ExceptionCode.ToString("X"));
-                Console.WriteLine("[+] ExceptionInfo.contextRecord.Eip 1:  \t0x{0}", ExceptionInfo.contextRecord.Rip.ToString("X"));
-        
-                unsafe
-                {
-                    var b = new testDel(test);
-                    IntPtr testPtr = Marshal.GetFunctionPointerForDelegate(b);
-                    Console.WriteLine("[+] Function test address:  \t\t\t0x{0}", testPtr.ToString("X"));
-                    ExceptionInfo.contextRecord.Rip = (IntPtr)testPtr;
-                }
-
-                Console.WriteLine("[+] ExceptionInfo.contextRecord.Eip 2: \t\t0x{0}", ExceptionInfo.contextRecord.Rip.ToString("X"));
-                Debug.WriteLine("[+] Debug 1");
-                // System.Threading.Thread.Sleep(30000);
+                Console.WriteLine("[+] The call was hooked, execute any code in here.");
                 return EXCEPTION_CONTINUE_EXECUTION;
             }
             return EXCEPTION_CONTINUE_SEARCH;
         }
-
 
 
         static void Main(string[] args)
@@ -308,13 +191,13 @@ namespace GuardPages
 
             IntPtr hookPtr = IntPtr.Zero;
             unsafe {
-                var aux_hook = new hookDel(hhandler);
+                var aux_hook = new hookDel(handler_function);
                 hookPtr = Marshal.GetFunctionPointerForDelegate(aux_hook);
             }
-            Console.WriteLine("[+] Function hhandler address: \t\t\t0x{0}", hookPtr.ToString("X"));
+            Console.WriteLine("[+] Function handler_function address: \t\t0x{0}", hookPtr.ToString("X"));
             AddVectoredExceptionHandler((uint)1, hookPtr);
             Console.WriteLine("\n[+] Calling MessageBoxA...");
-            MessageBox(0, "Test 2", "Test 2", 0);
+            MessageBox(0, "This call was hooked.", "Hook Test", 0);
         }
     }
 }
